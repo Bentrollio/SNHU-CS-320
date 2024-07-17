8146f27a8d5d9be81b5aa17580fd4b5a246bbfa3
Alex Baires <109702084+Bentrollio@users.noreply.github.com>
2024-07-17 10:29:17 -0400

    prototype contact system

--- ModuleThree/src/main/java/org/snhu/alexanderbaires/Contact.java
+++ ModuleThree/src/main/java/org/snhu/alexanderbaires/Contact.java
@@ -11,4 +11,68 @@
 package org.snhu.alexanderbaires;
 
 public class Contact {
+
+    String contactID;
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    String mailingAddress;
+
+    public Contact(String contactID, String firstName, String lastName, String phoneNumber, String mailingAddress) {
+        this.contactID = contactID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.mailingAddress = mailingAddress;
+    }
+
+    // Retrieves contactID
+    public String getContactID() {
+        return contactID;
+    }
+
+    // Updates and retrieves the customer's first name
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // Updates and retrieves the customer's last name
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Updates and retrieves the customer's phone number
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Updates and retrieves the customer's mailing address
+    public void setMailingAddress(String mailingAddress) {
+        this.mailingAddress = mailingAddress;
+    }
+
+    public String getMailingAddress() {
+        return mailingAddress;
+    }
+
+    // Prints object for verification purposes
+    public String toString() {
+        return contactID + "\n" + firstName + " " + lastName + "\n" +
+                phoneNumber + "\n" + mailingAddress;
+    }
+
+
+
 }
--- ModuleThree/src/main/java/org/snhu/alexanderbaires/ContactService.java
+++ ModuleThree/src/main/java/org/snhu/alexanderbaires/ContactService.java
@@ -8,10 +8,86 @@
  * 7-21-24
  */
 
-
-
 package org.snhu.alexanderbaires;
+import java.util.ArrayList;
+import java.util.Random;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ContactService {
 
+    ArrayList<Contact> contactList = new ArrayList<Contact>();
+
+    public void addContact(String firstName, String lastName, String phone, String address) {
+        String uniqueID = getHash(firstName, lastName);
+        Contact contact = new Contact(uniqueID, firstName, lastName, phone, address);
+        // VerifyContact
+        // CheckifContactExists
+        contactList.add(contact);
+
+    }
+
+    public void verifyContact() {
+
+    }
+
+    /**
+     * Adapted from the below stack overflow post:
+     *https://stackoverflow.com/questions/20536566/creating-a-random-string-with-a-z-and-0-9-in-java
+     *
+     * @return String randomKey
+     */
+
+    protected String getSaltString() {
+        String saltChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 10) { // length of the random string
+            int index = (int) (rnd.nextFloat() * saltChars.length());
+            salt.append(saltChars.charAt(index));
+        }
+        return salt.toString();
+    }
+
+    /**
+     * bytesToHex
+     *
+     * @param bytes - A byte array that will be converted into a HexString
+     * @return hex - A string containing the HexString converted message as output.
+     */
+    protected String bytesToHex(byte[] bytes) {
+        String hex;
+
+        // Converts byte array into HexString object
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < bytes.length; ++i) {
+            hexString.append(String.format("%02x", bytes[i]));
+        }
+        hex = hexString.toString();
+        return hex;
+    }
+
+    protected String getHash(String firstName, String lastName) {
+        String combinedString = firstName + lastName;
+        String checkSum = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(combinedString.getBytes());
+            byte[] digest = md.digest();
+
+            checkSum = bytesToHex(digest);
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return checkSum.substring(0, 10);
+    }
+
+    // FIXME: This function, while not in the parameters, is used for verification. Can be deleted in final
+    public void printContactList() {
+        for (Contact contact : contactList) {
+            System.out.println(contact);
+        }
+    }
 }
